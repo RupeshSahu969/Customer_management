30f1ac13db2043bf73a205e62c48c0372975745b
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-06-27 17:33:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+
+// import QuotePage from './components/QuotePage';
+
+function App() {
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
